7c9b4214f6a6597a116a41e47da9632026974a35
albertshkhyan <50060559+albertshkhyan@users.noreply.github.com>
2020-07-12 02:38:20 +0400

    express function (init express),handle get request static routes and dynamic routes, send (like res.end), sendFile

--- index.js
+++ index.js
@@ -5,14 +5,110 @@
 
 */
 const express = require('express');
-// Creates an Express application. The express() function is a top-level function exported by the express module.
+/* 
+express() - Creates an Express application. The express() function is a top-level function exported by the express module.
 
-//express like createServer
-const PORT = process.env.PORT || 3000;
-//process.env.PORT is undefined
-
+app.get() - handle get request on specify route
+    app.get("/", (req, res) => {
+        req and res have more functionality
+    })
+*/
 const app = express();
+const path = require("path");
 
-app.listen(PORT, () => {
-    console.log("Server is running...");
+/////////// static routes
+// app.get("/", (req, res) => {
+
+// });//cycling ➰➰ loop - we must close server response
+app.get("/", (req, res) => {
+    res.send();//like res.end()
 });
+
+
+/////////// dynamic routes (Path params)
+//ex : /news/12 or /news/5 etc <- dynamic route  ⚠ not only numbers, ex: /news/:hi -> Path params can be word
+// app.get("/news/:id", (req, res) => {
+//     // console.log('req.param', req.params);//{ id: '1' }
+//     res.send(`ID is - ${req.params.id}`)
+// });
+
+////several path params
+app.get("/news/:name/:id", (req, res) => {
+    console.log('req.param', req.params);//{ name: 'korona', id: '1' }
+    res.send(`name is - ${req.params.name} id is - ${req.params.id}`)
+});
+
+app.listen(3000);
+
+
+
+/****************************** ******************************/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/****************************** Show html files in browser  ******************************/
+////#sendFile   
+
+// //express like createServer
+// const PORT = process.env.PORT || 3000;
+// //process.env.PORT is undefined
+
+// // app.get('/', (req, res, next) => {})
+// app.get('/', (req, res) => {
+//     console.log('req', req);
+//     res.sendFile(path.join(__dirname, 'views', "index.html"));//Transfer the file at the given path., Automatically sets the Content-Type response header field.
+// });
+
+// app.get('/about', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'views', "about.html"));
+// });
+
+// app.listen(PORT, () => {
+//     console.log("Server is running...");
+// });
