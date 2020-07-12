8d86454302ff162baa036d0f80b4c51b51db797f
albertshkhyan <50060559+albertshkhyan@users.noreply.github.com>
2020-07-12 22:50:57 +0400

    app.use, express.static, create public folder and etc

--- index.js
+++ index.js
@@ -10,6 +10,17 @@ const hbs = exphbs.create({//return object
     defaultLayout: "main",
     extname: "hbs",
 });
+app.use(express.static('public'));
+/*
+* app.use([path,] callback [, callback...])
+    app.use() - Mounts the specified middleware function or functions at the specified path: the middleware function is executed when the base of the requested path matches path.
+    Монтирует указанную функцию промежуточного программного обеспечения или функции по указанному пути: функция промежуточного программного обеспечения выполняется, когда база запрошенного пути совпадает с путем.
+
+* express.static - This is a built-in middleware function in Express. It serves static files and is based on serve-static.
+
+Create a new middleware function to serve files from within a given root directory.
+
+*/
 
 //2 register in express that we have engine
 app.engine("hbs", hbs.engine);
