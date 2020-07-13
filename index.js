aa723762739de36960cc5adfe733db71d06192c7
albertshkhyan <50060559+albertshkhyan@users.noreply.github.com>
2020-07-13 19:22:00 +0400

    Routes, have error with route (/example/c), dubug nodeJS , and etc)

--- index.js
+++ index.js
@@ -1,55 +1,285 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
+const express = require("express");
 
-const app = express();
-const homeRouter = require("./routes/home");
-const coursesRouter = require("./routes/courses");
-const addRouter = require("./routes/add");
+const app = express();//instance of Express class
 
-const PORT = process.env.PORT || 3000;
 
-//1 cofig of engine
-const hbs = exphbs.create({//return object
-    defaultLayout: "main",
-    extname: "hbs",
-});
-app.use(express.static('public'));
 /*
-* app.use([path,] callback [, callback...])
-    app.use() - Mounts the specified middleware function or functions at the specified path: the middleware function is executed when the base of the requested path matches path.
-    Монтирует указанную функцию промежуточного программного обеспечения или функции по указанному пути: функция промежуточного программного обеспечения выполняется, когда база запрошенного пути совпадает с путем.
-
-* express.static - This is a built-in middleware function in Express. It serves static files and is based on serve-static.
-
-Create a new middleware function to serve files from within a given root directory.
-
+Routing (Маршрутизация)
+Routing determines how an application responds to a client request to a specific address (URI).
+|| Маршрутизация определяет, как приложение отвечает на клиентский запрос к конкретному адресу (URI).
 */
 
-//2 register in express that we have engine
-app.engine("hbs", hbs.engine);
-//3 set in express our gived cofig engine (with view engined)
-app.set("view engine", "hbs");
+/* -------------------------------------------------------------------------- */
+/*                                send and end                                */
+/* -------------------------------------------------------------------------- */
 
-//$1
-// app.get('/', (req, res) => {
-//     res.render("index", { title: "Home", isHome: true });
-// });
-// app.get('/courses', (req, res) => {
-//     res.render("courses", { title: "Courses", isCourses: true });
-// });
-// app.get('/add', (req, res) => {
-//     res.render("add", { title: "Add Courses", isAdd: true });
+// app.get("/", (req, res) => {
+//     console.log("Hello World");
+//     ////Sending a response of various types.
+//     // res.send({ some: 'json' });//Content-Type: application/json; charset=utf-8 ...
+//     // res.send("hi");//
+//     // res.end({ some: 'json' });//Error: chunk argument must be string ...
 // });
 
-// //$2
-// app.use(homeRouter);
-// app.use(coursesRouter);
-// app.use(addRouter);
+
+// app.listen(3000);
 
 
-// //$3 more readable
-app.use("/", homeRouter);
-app.use("/courses", coursesRouter);
-app.use("/add", addRouter);
 
-app.listen(PORT);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* -------------------------------------------------------------------------- */
+/*                               Route methods                                */
+/* -------------------------------------------------------------------------- */
+/*
+HTTP: get, post, put, head, delete, options, trace, copy, lock, mkcol, move, purge, propfind, proppatch, unlock, report, mkactivity, checkout, merge, m-search, notify, subscribe, unsubscribe, patch, search и connect.
+*/
+// // GET method route
+// app.get('/', function (req, res) {
+//     res.send('<h1>GET request to the homepage</h1>');
+//   });
+
+//   // POST method route
+//   app.post('/', function (req, res) {
+//     res.send('<h2>POST request to the homepage</h2>');
+//   });
+
+
+////chaining 
+// app
+//     .get('/', function (req, res) {
+//         res.send('<h1>GET request to the homepage</h1>');
+//     })
+//     .post('/', function (req, res) {
+//         res.send('<h2>POST request to the homepage</h2>');
+//     });
+
+
+// app.listen(3000);
+
+
+
+
+
+
+
+
+
+
+
+
+/* -------------------------------------------------------------------------- */
+/*                               Path of Routes                               */
+/* -------------------------------------------------------------------------- */
+
+// app.get('/', function (req, res) {
+//     res.send('Root route');
+// })
+// app.get('/ab?cd', function (req, res) {//http://localhost:3000/acd || http://localhost:3000/abcd ??
+//     res.send('ab?cd');
+// })
+
+// app.get('/random.text', function (req, res) {
+//     res.send('random.text');
+// });
+
+// app.get('/ab+cd', function(req, res) {//http://localhost:3000/abbcd || http://localhost:3000/abbbcd ???
+//     res.send('ab+cd');
+//   });
+
+// app.get('/ab*cd', function(req, res) {//http://localhost:3000/abhasdkjfkjsdhakfhsdkhijkcd - ab ... cd
+// res.send('ab*cd');
+// });
+
+// app.get('/ab(cd)?e', function (req, res) {//????
+//     res.send('ab(cd)?e');
+// });
+/**
+ * http://localhost:3000/abe -> work
+ * http://localhost:3000/abcde -> work
+ * http://localhost:3000/abee -> not work
+ */
+
+
+// app.get(/a/, function(req, res) {//This route path matches any element with “a” in the route name.
+//     res.send('/a/');
+//   });
+
+// app.get(/.*fly$/, function(req, res) {//last three letter must end by "fly"
+//     res.send('/.*fly$/');
+//   });
+/**
+ * http://localhost:3000/butterfly -> work
+ * http://localhost:3000/butterfly -> work
+ * http://localhost:3000/fly -> work
+ * http://localhost:3000/fl -> not work
+ */
+
+// app.listen(3000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* -------------------------------------------------------------------------- */
+/*                               Route handlers                               */
+/* -------------------------------------------------------------------------- */
+// app.get("/", (req, res) => {
+//     debugger
+//     res.send('Hello World');
+// });
+// app.get("/example/a", (req, res) => {
+//     res.send('Hello from A!');
+// });
+// app.get("/example/b", (req, res) => {
+//     res.send('Hello from B!');
+// });
+// app.listen(3000);
+
+/*
+* Для обработки запроса можно указать несколько функций обратного вызова, подобных middleware. Единственным исключением является то, что эти обратные вызовы могут инициировать next('route') для обхода остальных обратных вызовов маршрута. С помощью этого механизма можно включить в маршрут предварительные условия, а затем передать управление последующим маршрутам, если продолжать работу с текущим маршрутом не нужно.
+
+Обработчики маршрутов могут принимать форму функции, массива функций или их сочетания, как показано в примерах ниже.
+*/
+//// One route can be handled by several callback functions (be sure to specify the next object).
+////$1
+// app.get("/", (req, res) => {
+//     res.send('Hello World');
+// });
+// app.get("/example/a", (req, res, next) => {
+//     console.log('the response will be sent by the next function ...');
+//     next();//without next function infinity loop
+//     // res.end('Hello from A1!');//this is not show
+// }, (req, res) => {
+//     res.end('Hello from A2!');
+// });
+
+// app.listen(3000);
+
+
+
+////$2 - Find Error
+const cb0 = (req, res, next) => {
+    debugger
+    console.log('CB0');
+    next();
+}
+
+const cb1 = (req, res, next) => {
+    debugger
+    console.log('CB1');
+    next();
+}
+
+const cb2 = (req, res) => {
+    debugger
+    console.log('CB2');
+    next();
+}
+
+app.get('/example/c', [cb0, cb1, cb2], (req, res, next) => {//error
+    debugger
+    res.send('Hello from C!');
+});
+
+app.listen(3000);
+
+////work
+// console.log(1);
+// const cb0 = function (req, res, next) {
+//     debugger
+//     console.log('CB0');
+//     next();
+// }
+
+// const cb1 = function (req, res, next) {
+//     debugger
+//     console.log('CB1');
+//     next();
+// }
+// const cb2 = function (req, res, next) {
+//     debugger
+//     console.log('CB2');
+//     next();
+// }
+// const cb3 = function (req, res, next) {
+//     debugger
+//     console.log('CB3');
+//     next();
+// }
+
+// // app.get('/example/d', [cb0, cb1, cb2, cb3], function (req, res, next) {
+// //     console.log('the response will be sent by the next function ...');
+// //     next();
+// // }, function (req, res) {
+// //     res.send('Hello from D!');
+// // });
+// ////or
+// app.get('/example/d', [cb0, cb1, cb2, cb3], function (req, res, next) {
+//     debugger
+//     console.log('the response will be sent by the next function ...');
+//     res.send('Hello from D!');
+// });
+
+// app.listen(3000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+Why 3000 port not work
+
+Emitted 'error' event on Server instance at:
+    at emitErrorNT (net.js:1340:8)
+    at processTicksAndRejections (internal/process/task_queues.js:84:21) {
+  code: 'EADDRINUSE',
+  errno: 'EADDRINUSE',
+  syscall: 'listen',
+  address: '::',
+  port: 3000
+}
+*/
