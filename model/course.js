117932e540c25eacd3e9ba572c20df3be733cc33
albertshkhyan <50060559+albertshkhyan@users.noreply.github.com>
2020-07-14 18:54:49 +0400

    MVC pattern (module), db imitation, add course, uuid packag, and etc

---
+++ model/course.js
@@ -0,0 +1,50 @@
+const uuid = require("uuid");
+const fs = require("fs");
+const path = require("path");
+const { rejects } = require("assert");
+
+class Course {
+    constructor(title, price, image) {
+        this.title = title;
+        this.price = price;
+        this.image = image;
+        this.id = uuid.v4();
+    }
+
+    toJson() {
+        return ({
+            title: this.title,
+            price: this.price,
+            image: this.image,
+            id: this.id
+        });
+    }
+
+    async save() {
+        const course = await Course.getAll();
+        // course.push( JSON.stringify(this.toJson()) )
+
+        return new Promise((res, rej) => {
+            //set file in data/course.json
+            fs.writeFile(
+                path.join(__dirname, "../data", "course.json"),
+                JSON.stringify(this.toJson()),
+                (err) => {
+                    if (err) rej(err);
+                    else { res() }
+                }
+            )
+        });
+    }
+
+    static getAll() {
+        return new Promise((res, rej) => {
+            fs.readFile(path.join(__dirname, '../data', "course.json"), "utf-8", (err, content) => {
+                if (err) rej(err);
+                else { res(JSON.parse(content)) }//parsing "[]" for can do push in save
+            });
+        });
+
+    }
+}
+module.exports = Course;
--- routes/add.js
+++ routes/add.js
@@ -1,21 +1,22 @@
 const { Router } = require("express");
-const { route } = require("./home");
-
-
 const router = Router();
+
+const Course = require("../model/course");
 
 router.get('/', (req, res) => {
     res.render("add", { title: "Add Courses", isAdd: true });
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     console.log('req.body', req.body);//undefined
-/*for fix req.body: we must add middleware which will parse request url to object 
-title=Alik&price=Shkhyan&image=ssd -> { title: 'Alik', price: 'Shkhyan', image: 'ssd' }
-*/
-////after add middleware express.urlencoded
-console.log('req.body', req.body);//{ title: 'Alik', price: 'Shkhyan', image: 'ssd' }
-
+    /*for fix req.body: we must add middleware which will parse request url to object 
+    title=Alik&price=Shkhyan&image=ssd -> { title: 'Alik', price: 'Shkhyan', image: 'ssd' }
+    */
+    ////after add middleware express.urlencoded
+    // console.log('req.body', req.body);//{ title: 'Alik', price: 'Shkhyan', image: 'ssd' }
+    const { title, price, image } = req.body;
+    const course = await new Course(title, price, image);//create json, if you want to add in DB call save method
+    course.save();
 
     res.redirect('/courses');
 })
