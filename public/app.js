4b1a1a7726643a76977fd7d5b2a8f67638f5382d
albertshkhyan <50060559+albertshkhyan@users.noreply.github.com>
2020-07-15 02:48:54 +0400

    create new static js  file -> app.js (scripts for client), in foo.hbs connect app.js, and etc

---
+++ public/app.js
@@ -0,0 +1,10 @@
+////in here will be  clients scripts, in here is Javascript
+// console.log('document', document);//show in browse
+
+document.querySelectorAll(".price").forEach((node) => {
+    node.textContent = new Intl.NumberFormat("am-AM", {
+        style: 'currency', currency: 'arm'
+    }).format(node.textContent);
+});
+
+
