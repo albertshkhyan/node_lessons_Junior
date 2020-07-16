902d62d5e0b7d0b3a5063a559bd4a89b00b08d9a
albertshkhyan <50060559+albertshkhyan@users.noreply.github.com>
2020-07-16 18:46:34 +0400

    alternate version, in courses.js add .post(/) -> .post(/edit) and in courseEdit.hbs change action=/courses -> action=courses/edt)

--- routes/courses.js
+++ routes/courses.js
@@ -1,4 +1,4 @@
-const {Router} = require("express");
+const { Router } = require("express");
 const Course = require("../model/course");
 
 
@@ -6,29 +6,28 @@ const router = Router();
 
 
 router.get('/', async (req, res) => {
-    const course = await Course.getAll();        
+    const course = await Course.getAll();
     res.render("courses", { title: "Courses", isCourses: true, course });
 
 });
 
 router.get("/:id/edit", async (req, res) => {
-        // console.log('req,query', req.query);//{ allow: 'true' }
-        //If user has allow edit course
-        if(!req.query.allow) {
-            //return for not to continue the code
-            return res.redirect('/');
-        }
-        const course = await Course.getById(req.params.id);
-        console.log('course', course);
+    // console.log('req,query', req.query);//{ allow: 'true' }
+    //If user has allow edit course
+    if (!req.query.allow) {
+        //return for not to continue the code
+        return res.redirect('/');
+    }
+    const course = await Course.getById(req.params.id);
 
-        res.render("courseEdit", {
-            title : `${course.title}`,
-            course
-        });
+    res.render("courseEdit", {
+        title: `${course.title}`,
+        course
+    });
 });
 
 
-router.post("/", async (req, res) => {
+router.post("/edit", async (req, res) => {
     //req bosy is values of input (have 4 key), id is static, || id is for check, find which object did change
     // console.log('req.body', req.body);
     await Course.update(req.body);
@@ -37,9 +36,9 @@ router.post("/", async (req, res) => {
 
 router.get('/:id', async (req, res) => {
     const course = await Course.getById(req.params.id);
-    res.render("course", { 
+    res.render("course", {
         layout: "empty", //which layout  we will push own course.hbs content
-        title : `Course ${course.title}`,
+        title: `Course ${course.title}`,
         course
     });
 });
