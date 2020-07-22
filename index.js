b5ac09966621819a96594805c89e5129de172ef2
albertshkhyan <50060559+albertshkhyan@users.noreply.github.com>
2020-07-22 17:32:23 +0400

    add useFindAndModify property false, create user model (ref option, Schema.Type,ObjectId) and etc)

--- index.js
+++ index.js
@@ -8,10 +8,8 @@ const homeRouter = require("./routes/home");
 const coursesRouter = require("./routes/courses");
 const addRouter = require("./routes/add");
 const cartRouter = require('./routes/cart');
-
 //mongoose
 const mongoose = require("mongoose");
-
 
 
 //1 cofig of engine
@@ -19,8 +17,9 @@ const hbs = exphbs.create({//return object
     defaultLayout: "main",
     extname: "hbs",
 });
-app.use(express.static('public'));
-////When do post request req.body is undefined -> 
+
+app.use(express.static(__dirname + '/public'));//Create a new middleware function to serve files from within a given root directory
+
 app.use(express.urlencoded({ extended: true }));//true-> qs lib, parse req body <- working
 
 //2 register in express that we have engine
@@ -37,27 +36,20 @@ app
     .use("/add", addRouter)
     .use("/courses", addRouter);
 
+// Database Connection 
 
-const start = async () => {
-    const password = "8Ps8wL2HvHkSzODP";
-    const url = `mongodb+srv://alik:${password}@cluster0.mpuj4.mongodb.net/shop?retryWrites=true&w=majority`;
-
-    //connect return promise for this we must wait, first connect mongoose to MongoDB, use await
-    //but can work reject, for handle error we will use try catch
+(async () => {
     try {
-        await mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
-        //check connected or not
+        const url = `mongodb+srv://alik:8Ps8wL2HvHkSzODP@cluster0.mpuj4.mongodb.net/shop?retryWrites=true&w=majority`;
+        await mongoose.connect(url, {
+            useNewUrlParser: true,//By default, mongoose.connect() will print out the below warning:
+            useUnifiedTopology: true,
+            useFindAndModify: false
+        });//connect on mongoDB
         const PORT = process.env.PORT || 3000;
         app.listen(PORT);
     } catch (err) {
         //if connect call reject
         console.log('err', err);
     }
-
-}
-
-start();
-
-//mongodb+srv://alik:<password>@cluster0.mpuj4.mongodb.net/<dbname>?retryWrites=true&w=majority
-// const current_ip = "37.252.94.176";
-
+})();
