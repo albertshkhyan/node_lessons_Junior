fa3cb37ae8f29ab3941bd1b1d814b9883e15353b
albertshkhyan <50060559+albertshkhyan@users.noreply.github.com>
2020-07-26 20:43:54 +0400

    add instance method -> removeItemsFromCartd, have bug in app.js, incorrect id, and etc

--- model/user.js
+++ model/user.js
@@ -52,110 +52,23 @@ user.methods.addCourseInCart = function (course) {
     return this.save();
 }
 
+user.methods.removeItemsFromCart = function (id) {
+    let items = [...this.cart.items];
+
+    const userCart = items.find(c => c.courseId.toString() === id.toString());
+    // console.log('userCar***********t', userCart);
+
+    if (userCart.count === 1) {
+        items = items.filter(c => c.courseId.toString() !== id.toString())
+    }
+    else {
+        userCart.count--
+    }
+
+    this.cart = { items };
+    return this.save();
+}
+
 module.exports = model("User", user, 'users');//compile user schema to model, Returns another Model instance.
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-const mongoose= require("mongoose");
-const { Schema, model } = require("mongoose");
-
-// Database Connection  1
-(async () => {
-    try {
-        const url = `mongodb+srv://alik:8Ps8wL2HvHkSzODP@cluster0.mpuj4.mongodb.net/shop?retryWrites=true&w=majority`;
-        await mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });//connect on mongoDB
-        const PORT = process.env.PORT || 3000;
-        // app.listen(PORT);
-    } catch (err) {
-        //if connect call reject
-        console.log('err', err);
-    }
-})();
-// define Schema 2
-const user = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    //Every user have own cart
-    cart: {
-        items: [
-            //elements of items array must have count key
-            {
-                count: {
-                    type: Number,
-                    required: true,
-                    default: 1
-                }
-            }
-        ]
-    }
-});
-
-
-const User = model("User", user, 'users');//compile user schema to model, Returns another Model instance.//3
-// console.log('User', User);
-
-//not show in db
-// koko.save(function (err) {
-//     if (err) return handleError(err);
-//     // saved!
-//   })
-const users = new User({//4
-    name : "John",
-    email : "john@mail.ru",
-    items: [{
-        count: 3
-    }]
-});
-
-users.save(function(err,result){ //5
-    if (err){
-        console.log(err);
-    }
-    else{
-        console.log(result)
-    }
-})
-*/
--- routes/cart.js
+++ routes/cart.js
@@ -8,19 +8,10 @@ const User = require("../model/user");
 const router = Router();
 ////HELPER FUNCTIONS
 const mapCartItems = (course) => {
-    // console.log('course', course);
     return course.map((c) => {
-        // console.log('c.courseId._doc', c.courseId._doc);//identy object show
-        // console.log('c.courseId', c.courseId);//identy object show
-        // console.log(' c.courseId === c.courseId._doc',  c.courseId === c.courseId._doc);//false
-
-        // console.log('"toJSON" in c', "toJSON" in c);//true
-        // console.log('"toJSON" in c.courseId', "toJSON" in c.courseId);//true
         return ({
-            // ...c.courseId._doc,  count: c.count//👌 not show metada wiht _doc, but... more correct is toJSON()
+            // ...c.courseId._doc,  count: c.count//not recomented
             ...c.courseId.toJSON(), count: c.count//✅
-            ////Without toJSON or _doc
-            // ...c.courseId,  count: c.count//show metadata, return array of map have metadat without _doc or toJSON👎
         })
     });
 }
@@ -30,33 +21,9 @@ const mapTotalPriceOfItems = (course) => {
 
 router.get('/', async (req, res) => {
     try {
-        // const cart = await Cart.fetch();//get content of cart.json
-        // res.render('cart', {
-        //     isCart: true,
-        //     courses: cart.courses,
-        //     price: cart.price
-        // });
         const user = await req.user
             .populate("cart.items.courseId")
             .execPopulate();//without execPopulate can't get content of Course model
-
-
-        // console.log('mapCartItems ////////////////', mapCartItems(user.cart.items));
-
-
-        // res.json({
-        //     getCart: true
-        // })
-        //user can be a lot  for this we musth use map
-
-        // console.log('user.cart.items.courseId', user.cart.items.courseId);//no have  metadata
-        // console.log('user.cart.items', user.cart.items[0]);//no have  metadata
-        // console.log('user.cart.items[0]', user.cart.items[0]);//no have  metadata
-        // console.log('user.cart.items[0].courseId', user.cart.items[0].courseId);//no have  metadata
-
-        // console.log('mapCartItems(user.cart.items).courseId', mapCartItems(user.cart.items));
-
-        // console.log('mapTotalPriceOfItems(user.cart.items)', mapTotalPriceOfItems(user.cart.items));//reducer not return array
 
         res.render("cart", {
             title: "Cart",
@@ -71,26 +38,34 @@ router.get('/', async (req, res) => {
 });
 
 router.delete("/remove/:id", async (req, res) => {
-    // const cart = await Cart.remove(req.params.id);
-    res
-        .status(200)
-        .json(cart);
+    console.log('delete ++++++++++ req.params.id', req.params.id);
+
+    try {
+        const user = await req.user.removeItemsFromCart(req.params.id);
+        const getCoursesByid = await req.user.populate('cart.items.courseId').execPopulate();
+
+        const cart = {
+            courses: mapCartItems(getCoursesByid.cart.items),
+            price: mapTotalPriceOfItems(getCoursesByid.cart.items)
+
+        }
+        res
+            .status(200)
+            .json(cart);
+    } catch (err) {
+        console.log('err', err);
+    }
 });
 
 router.post('/add', async (req, res) => {
     try {
         const course = await Course.findById(req.body.id);
-        // const user = new User();
-        // await user.addCourseInCart(course);
-        ////or
         await req.user.addCourseInCart(course);
-
         // res.json({ added: true });
         res.redirect('/cart');//After adding of course in basket we redirect to card
     }
     catch (err) {
         console.log('err', err);
-
     }
 
 });
